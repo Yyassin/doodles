0db7d9cc452eb22e115d8140e54e7cffd36115e7
Abdalla Abdelhadi <abdalla.abdelhadi02@gmail.com>
2023-10-10 20:47:33 -0400

    create websocket server and client

--- node/src/app.ts
+++ node/src/app.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import http from 'http';
+import setUpWSS from './websockets';
+
 const app = express();
 const port = 3005;
+const server = http.createServer(app);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+setUpWSS(server);
