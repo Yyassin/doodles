92214ffa242304eed866dc0f6c475118c75077bb
thedana1 <danaelsherif1@gmail.com>
2023-10-11 01:43:13 -0400

    Fixed Menu Spacing

--- client/src/views/DropDownMenu.tsx
+++ client/src/views/DropDownMenu.tsx
@@ -50,52 +50,52 @@ const DropDownMenu = () => {
         <DropdownMenu.Portal>
           <DropdownMenu.Content
             className="min-w-[220px] bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
-            sideOffset={5}
+            sideOffset={9}
           >
             <DropdownMenu.Item
-              className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
+              className="group text-[13px] indent-[10px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
               onClick={handleOpen}
             >
               <EnvelopeOpenIcon /> Open
             </DropdownMenu.Item>
             <DropdownMenu.Separator className="h-[1px] bg-neutral-200 m-[5px]" />
             <DropdownMenu.Item
-              className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
+              className="group text-[13px] indent-[10px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
               onClick={handleSave}
             >
-              <DownloadIcon /> Save to...
+              <DownloadIcon /> Save
             </DropdownMenu.Item>
             <DropdownMenu.Separator className="h-[1px] bg-neutral-200 m-[5px]" />
             <DropdownMenu.Item
-              className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
+              className="group text-[13px] indent-[10px] rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
               onClick={handleExport}
             >
-              <Share2Icon /> Export Drawing...
+              <Share2Icon /> Export
             </DropdownMenu.Item>
             <DropdownMenu.Separator className="h-[1px] bg-neutral-200 m-[5px]" />
             <DropdownMenu.Item
-              className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
+              className="group text-[13px] indent-[10px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none  data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
               onClick={handleLive}
             >
               <Share1Icon /> Live Collaboration
             </DropdownMenu.Item>
             <DropdownMenu.Separator className="h-[1px] bg-neutral-200 m-[5px]" />
             <DropdownMenu.Item
-              className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
+              className="group text-[13px] indent-[10px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
               onClick={handleInfo}
             >
               <InfoCircledIcon /> Info
             </DropdownMenu.Item>
             <DropdownMenu.Separator className="h-[1px] bg-neutral-200 m-[5px]" />
             <DropdownMenu.Item
-              className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
+              className="group text-[13px] indent-[10px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
               onClick={handleReset}
             >
               <TrashIcon /> Reset Canvas
             </DropdownMenu.Item>
             <DropdownMenu.Separator className="h-[1px] bg-neutral-200 m-[5px]" />
             <DropdownMenu.Item
-              className="group text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
+              className="group text-[13px] indent-[10px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] px-[5px] relative pl-[25px] select-none outline-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:bg-violet9 data-[highlighted]:text-white hover:bg-teal-500"
               onClick={handleEditCanvas}
             >
               <Pencil2Icon /> Edit Canvas
