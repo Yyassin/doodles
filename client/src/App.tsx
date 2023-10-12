cdedf085bc426d81a025e416bc2d8d9b5c43b26f
thedana1 <danaelsherif1@gmail.com>
2023-10-11 21:34:03 -0400

    Highlight button when selected

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-// import SignUp from './views/SignUpPage';
-// import Dashboard from './views/Dashboard';
-// import LoginPage from './views/SignInPage';
+import SignUp from './views/SignUpPage';
+import Dashboard from './views/Dashboard';
+import LoginPage from './views/SignInPage';
 import ToolbarDemo from './views/ToolBar';
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  return <ToolbarDemo />;
-
-  // return (
-  //   <>
-  //     <Router>
-  //       <Routes>
-  //         <Route path="/*" Component={SignUp} />
-  //         <Route path="/dashboard" Component={Dashboard} />
-  //         <Route path="/signup" Component={SignUp} />
-  //         <Route path="/signin" Component={LoginPage} />
-  //       </Routes>
-  //     </Router>
-  //   </>
-  // );
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/*" Component={SignUp} />
+          <Route path="/dashboard" Component={Dashboard} />
+          <Route path="/toolbar" Component={ToolbarDemo} />
+          <Route path="/signup" Component={SignUp} />
+          <Route path="/signin" Component={LoginPage} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
