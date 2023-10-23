c5a4b83f11698d3d0f318f52b0f344e927678dbb
Zakariyya Almalki <zackzouk@gmail.com>
2023-10-22 21:09:39 -0400

    DOO36 - Logout button and functionality (#23)
    * DOO36 - Logout button and functionality
    
    * making logout button its own component

--- client/src/views/Dashboard.tsx
+++ client/src/views/Dashboard.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useAppStore } from '@/stores/AppStore';
+import LogoutButton from '../components/lib/LogoutButton';
 
 export default function Dashboard() {
   const { setMode } = useAppStore(['setMode']);
+
   return (
     <div>
       <h1>Dashboard dummy text</h1>
       <button onClick={() => setMode('canvas')}>Canvas</button>
+      <LogoutButton />
     </div>
   );
 }
