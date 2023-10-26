ec42a0248e047ff84eaef64eda9a949008d2e14e
Zakariyya Almalki <zackzouk@gmail.com>
2023-10-26 16:36:26 -0400

    DOO-45: form to input user info before sign up

--- client/tests/stores/SignUp.test.tsx
+++ client/tests/stores/SignUp.test.tsx
@@ -9,11 +9,22 @@ import { signup } from '../../src/views/SignUpPage';
 describe('signup', () => {
   it('should sign up given user', async () => {
     try {
-      const user = await signup('test3453@gmail.com', 'password'); //sign in test user with test email/pass
-      expect(true).toBe(true); //it worked, they signed in
-      expect(user.user).toBeDefined(); //make sure user exist
-      expect(user.user.email).toBe('test3453@gmail.com'); //make sure email is correct
-      await user.user.delete(); //delete user so test will work next time
+      const userCredential = await signup(
+        'test3453@gmail.com',
+        'password',
+        'test',
+        'testLast',
+        null,
+      ); // Sign up test user with test email/pass
+
+      const user = userCredential.user; // Access the user object from the userCredential
+
+      expect(true).toBe(true); // It worked, they signed in
+      expect(user).toBeDefined(); // Make sure user exists
+      expect(user.email).toBe('test3453@gmail.com'); // Make sure email is correct
+
+      // Delete the user so the test will work next time
+      await user.delete();
     } catch (error) {
       expect(true).toBe(false); //failed, fail the test
     }
