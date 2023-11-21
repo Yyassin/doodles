44708662b4f7cb9e810b5636a3459c201e73cce4
Dana El Sherif <89320833+TheDana1@users.noreply.github.com>
2023-11-20 20:09:46 -0500

    Doo66 reset canvas (#38)
    * changed colour & added reset
    
    * Done
    
    * small edit

--- client/src/stores/CanvasElementsStore.ts
+++ client/src/stores/CanvasElementsStore.ts
@@ -60,6 +60,7 @@ interface CanvasElementActions {
   setSelectedElement: (id: string) => void;
   undoCanvasHistory: () => void;
   pushCanvasHistory: () => void;
+  resetCanvas: () => void;
   redoCanvasHistory: () => void;
   setCanvasElementState: (elment: CanvasElementState) => void;
 }
@@ -374,6 +375,19 @@ const redoCanvasHistory = (set: SetState<CanvasElementState>) => () => {
     set(prevState);
   }
 };
+
+/**
+ * Sets the canvas state to inital state
+ * Erases history
+ * @returns reset state
+ */
+const resetCanvas = (set: SetState<CanvasElementState>) => () => {
+  historyIndex = 0;
+  history = [initialCanvasElementState];
+  const state = initialCanvasElementState;
+  set(state);
+};
+
 /**
  * Sets the currently selected element id.
  * @param selectedElementId The id to set as selected.
@@ -440,6 +454,7 @@ const canvasElementStore = create<CanvasElementStore>()((set) => ({
   undoCanvasHistory: undoCanvasHistory(set),
   pushCanvasHistory: pushCanvasHistory(set),
   redoCanvasHistory: redoCanvasHistory(set),
+  resetCanvas: resetCanvas(set),
 }));
 export const useCanvasElementStore =
   createStoreWithSelectors(canvasElementStore);
