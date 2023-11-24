5f04e7e1431a32a78423665fa7ae40f77416d832
Yousef Yassin <you.ayassin@gmail.com>
2023-11-24 08:51:18 -0500

    Zoom with buttons and scroll. (#42)

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,2 +1,14 @@
+/**
+ * Various constants used throughout the application.
+ */
+
+// Websockets
 const WS_PORT = 3005 as number;
 export const WS_URL = `ws://localhost:${WS_PORT}`;
+
+// Zoom Constraints
+export const ZOOM = {
+  INC: 0.01,
+  MIN: 0.1, // 10%
+  MAX: 20, // 2000%
+} as const;
