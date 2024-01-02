5231641c863dd7ec6852ca4e482d55458f6c8b5d
Ibrahim Almalki <ibrahimalmalki@cmail.carleton.ca>
2024-01-01 19:04:30 -0500

    thickness

--- client/src/components/lib/CustomizabilityToolbar.tsx
+++ client/src/components/lib/CustomizabilityToolbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as Toolbar from '@radix-ui/react-toolbar';
 import ToolGroup, { colourType } from './ChangeColor';
+import StrokeToolGroup, { strokeType } from './ChangeThickness';
 
 /**
  * The toolbar that is displayed on the canvas.
@@ -8,6 +9,7 @@ import ToolGroup, { colourType } from './ChangeColor';
 const CustomToolbar = () => {
   return (
     <Toolbar.Root className="p-[0.3rem] gap-[0.3rem] min-w-max rounded-lg bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] mt-40 absolute ml-3">
+      {/* Stroke Color */}
       <h2 className="text-sm font-semibold mb-2">Color</h2>{' '}
       <Toolbar.Separator className="w-[1px] bg-neutral-200 mx-[0.2rem]" />
       <ToolGroup
@@ -21,6 +23,10 @@ const CustomToolbar = () => {
           ] as colourType[]
         }
       />
+      {/* Stroke Thickness */}
+      <h2 className="text-sm font-semibold mb-2">Stroke Thickness</h2>{' '}
+      <Toolbar.Separator className="w-[1px] bg-neutral-200 mx-[0.2rem]" />
+      <StrokeToolGroup tools={['thin', 'bold', 'extraBold'] as strokeType[]} />
     </Toolbar.Root>
   );
 };
