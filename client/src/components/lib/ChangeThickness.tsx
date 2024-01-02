3cafbbe56b651c0df013fa100c1d48a9aceb88da
Ibrahim Almalki <ibrahimalmalki@cmail.carleton.ca>
2024-01-01 21:10:16 -0500

    thickness + color

--- client/src/components/lib/ChangeThickness.tsx
+++ client/src/components/lib/ChangeThickness.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
-import { capitalize } from '@/lib/misc';
-import IconButton from './IconButton';
+import React from 'react';
 import { useCanvasElementStore } from '@/stores/CanvasElementsStore';
-import { createElement } from '@/lib/canvasElements/canvasElementUtils';
+import ToolButton from './ToolButtonSelector';
 
 /** Customizability Toolbar */
 const strokeTypes = ['thin', 'bold', 'extraBold'] as const;
@@ -17,92 +15,7 @@ const strokeMap: Record<string, string> = {
 const mapStroke = {
   thin: 1.25,
   bold: 3,
-  extraBold: 5,
-};
-
-/**
- * Defines a button for a toolbar tool. Contains
- * a representative icon (defined above) and
- * a state hook to set the selected tool. The currently
- * active tool is highlighted.
- * @param tool The tool the button corresponds to.
- * @param active True if this tool is currently selected, false otherwise.
- * @param children Children React nodes, if any.
- */
-const ToolButton = ({
-  tool,
-  active,
-  children,
-  setStrokeTool,
-}: {
-  tool: strokeType;
-  active: boolean;
-  children?: React.ReactNode;
-  setStrokeTool: React.Dispatch<strokeType>;
-}) => {
-  const {
-    editCanvasElement,
-    selectedElementId,
-    fillColors,
-    types,
-    strokeColors,
-    bowings,
-    roughnesses,
-    //strokeWidths,
-    fillStyles,
-    strokeLineDashes,
-    opacities,
-    p1,
-    p2,
-    textStrings,
-  } = useCanvasElementStore([
-    'editCanvasElement',
-    'selectedElementId',
-    'fillColors',
-    'types',
-    'strokeColors',
-    'bowings',
-    'roughnesses',
-    'strokeWidths',
-    'fillStyles',
-    'strokeLineDashes',
-    'opacities',
-    'freehandPoints',
-    'p1',
-    'p2',
-    'textStrings',
-  ]);
-
-  const onClick = () => {
-    setStrokeTool(tool);
-    const newElement = createElement(
-      selectedElementId,
-      p1[selectedElementId].x,
-      p1[selectedElementId].y,
-      p2[selectedElementId].x,
-      p2[selectedElementId].y,
-      types[selectedElementId],
-      undefined,
-      {
-        stroke: strokeColors[selectedElementId],
-        fill: fillColors[selectedElementId],
-        bowing: bowings[selectedElementId],
-        roughness: roughnesses[selectedElementId],
-        strokeWidth: mapStroke[tool],
-        fillStyle: fillStyles[selectedElementId],
-        strokeLineDash: strokeLineDashes[selectedElementId],
-        opacity: opacities[selectedElementId],
-        text: textStrings[selectedElementId],
-      },
-    );
-    editCanvasElement(selectedElementId, newElement);
-  };
-
-  return (
-    <IconButton label={capitalize(tool)} active={active} onClick={onClick}>
-      {children}
-    </IconButton>
-  );
+  extraBold: 7,
 };
 
 /**
@@ -113,20 +26,18 @@ const ToolButton = ({
  * to highlight the selected tool.
  */
 const StrokeToolGroup = ({ tools }: { tools: strokeType[] }) => {
-  const { strokeWidths, selectedElementId } = useCanvasElementStore([
+  const { strokeWidths, selectedElementIds } = useCanvasElementStore([
     'strokeWidths',
-    'selectedElementId',
+    'selectedElementIds',
   ]);
-  const [strokeTool, setStrokeTool] = useState(strokeWidths[selectedElementId]);
 
   return (
     <div className="flex">
       {tools.map((toolName) => (
         <div key={`CustomToolbar-${toolName}`} className="relative">
           <ToolButton
-            tool={toolName}
-            active={strokeTool === mapStroke[toolName]}
-            setStrokeTool={setStrokeTool as React.Dispatch<strokeType>}
+            customizabilityDict={{ strokeWidth: mapStroke[toolName] }}
+            active={strokeWidths[selectedElementIds[0]] === mapStroke[toolName]}
           >
             <div className={'w-5 h-5 square-full ' + strokeMap[toolName]}></div>
           </ToolButton>
