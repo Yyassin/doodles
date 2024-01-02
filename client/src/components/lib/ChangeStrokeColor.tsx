f1bead1404461de7f4a5c168195ad69dc287f50f
Ibrahim Almalki <ibrahimalmalki@cmail.carleton.ca>
2024-01-01 22:10:09 -0500

    stroke color (border for shapes)

---
+++ client/src/components/lib/ChangeStrokeColor.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useCanvasElementStore } from '@/stores/CanvasElementsStore';
+import ToolButton from './ToolButtonSelector';
+
+/** Customizability Toolbar */
+const strokeColourTypes = [
+  'redCircle',
+  'greenCircle',
+  'blueCircle',
+  'orangeCircle',
+  'blackCircle',
+] as const;
+export type strokeColourType = (typeof strokeColourTypes)[number];
+
+const strokeColorMap: Record<string, string> = {
+  redCircle: 'bg-red-500',
+  greenCircle: 'bg-green-500',
+  blueCircle: 'bg-blue-500',
+  orangeCircle: 'bg-orange-500',
+  blackCircle: 'bg-black',
+};
+
+const mapStrokeColour = {
+  redCircle: '#FF0000',
+  greenCircle: '#008000',
+  blueCircle: '#0000FF',
+  orangeCircle: '#FFA500',
+  blackCircle: '#000000',
+};
+
+/**
+ * Creates a row group of buttons corresponding
+ * to the provided list of tools.
+ * @param tools The list of tools
+ * @param selectedTool The currently selected tool, used
+ * to highlight the selected tool.
+ */
+const StokeColorToolGroup = ({ tools }: { tools: strokeColourType[] }) => {
+  const { strokeColors, selectedElementIds } = useCanvasElementStore([
+    'strokeColors',
+    'selectedElementIds',
+  ]);
+
+  return (
+    <div className="flex">
+      {tools.map((toolName) => (
+        <div key={`CustomToolbar-${toolName}`} className="relative">
+          <ToolButton
+            customizabilityDict={{ strokeColor: mapStrokeColour[toolName] }}
+            active={
+              strokeColors[selectedElementIds[0]] === mapStrokeColour[toolName]
+            }
+          >
+            <div
+              className={'w-5 h-5 rounded-full ' + strokeColorMap[toolName]}
+            ></div>
+          </ToolButton>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default StokeColorToolGroup;
--- client/src/components/lib/CustomizabilityToolbar.tsx
+++ client/src/components/lib/CustomizabilityToolbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import * as Toolbar from '@radix-ui/react-toolbar';
 import ToolGroup, { colourType } from './ChangeColor';
 import StrokeToolGroup, { strokeType } from './ChangeThickness';
+import StrokeColorToolGroup, { strokeColourType } from './ChangeStrokeColor';
 
 /**
  * The toolbar that is displayed on the canvas.
@@ -9,7 +10,7 @@ import StrokeToolGroup, { strokeType } from './ChangeThickness';
 const CustomToolbar = () => {
   return (
     <Toolbar.Root className="p-[0.3rem] gap-[0.3rem] min-w-max rounded-lg bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)] mt-40 absolute ml-3">
-      {/* Stroke Color */}
+      {/* Background Color */}
       <h2 className="text-sm font-semibold mb-2">Color</h2>{' '}
       <Toolbar.Separator className="w-[1px] bg-neutral-200 mx-[0.2rem]" />
       <ToolGroup
@@ -27,6 +28,20 @@ const CustomToolbar = () => {
       <h2 className="text-sm font-semibold mb-2">Stroke Thickness</h2>{' '}
       <Toolbar.Separator className="w-[1px] bg-neutral-200 mx-[0.2rem]" />
       <StrokeToolGroup tools={['thin', 'bold', 'extraBold'] as strokeType[]} />
+      {/* Stroke Color (border for shapes) */}
+      <h2 className="text-sm font-semibold mb-2">Stroke Color</h2>{' '}
+      <Toolbar.Separator className="w-[1px] bg-neutral-200 mx-[0.2rem]" />
+      <StrokeColorToolGroup
+        tools={
+          [
+            'redCircle',
+            'greenCircle',
+            'blueCircle',
+            'orangeCircle',
+            'blackCircle',
+          ] as strokeColourType[]
+        }
+      />
     </Toolbar.Root>
   );
 };
