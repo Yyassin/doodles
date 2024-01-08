60995abf9df3ac185e0ef1624696edacb468e1d3
yyassin <you.ayassin@gmail.com>
2024-01-07 20:56:55 -0500

    Fix d.ts type definitions.

--- client/src/global.d.ts
+++ client/src/global.d.ts
@@ -8,35 +8,3 @@ declare global {
     };
   }
 }
-
-/**
- * Extensions to the API types for image-blob-reduce compatibility
- * with pica: https://github.com/nodeca/image-blob-reduce/issues/23#issuecomment-783271848
- */
-declare module 'image-blob-reduce' {
-  import { PicaResizeOptions, Pica } from 'pica';
-  namespace ImageBlobReduce {
-    interface ImageBlobReduce {
-      toBlob(file: File, options: ImageBlobReduceOptions): Promise<Blob>;
-      _create_blob(
-        this: { pica: Pica },
-        env: {
-          out_canvas: HTMLCanvasElement;
-          out_blob: Blob;
-        },
-      ): Promise<unknown>;
-    }
-
-    interface ImageBlobReduceStatic {
-      new (options?: unknown): ImageBlobReduce;
-
-      (options?: unknown): ImageBlobReduce;
-    }
-
-    interface ImageBlobReduceOptions extends PicaResizeOptions {
-      max: number;
-    }
-  }
-  const reduce: ImageBlobReduce.ImageBlobReduceStatic;
-  export = reduce;
-}
