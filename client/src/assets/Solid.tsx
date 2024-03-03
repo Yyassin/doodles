67d8b720567f63b784c0e2bba65dfaaa9703fb69
Ibrahim Almalki <67344551+Eebro@users.noreply.github.com>
2024-03-03 18:48:18 -0500

    replace fill styles with svgs (#80)

---
+++ client/src/assets/Solid.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const SolidSVG: React.FC = () => (
+  <svg
+    version="1.0"
+    xmlns="http://www.w3.org/2000/svg"
+    width="2em"
+    height="2em"
+    viewBox="0 0 150 150"
+  >
+    <g
+      transform="translate(-25.000000,160.000000) scale(0.1100000,-0.1100000)"
+      fill="#000000"
+      stroke="none"
+    >
+      <path
+        d="M865 1434 c-130 -19 -217 -48 -293 -95 -351 -216 -406 -707 -112
+-999 119 -118 247 -176 415 -187 107 -7 214 16 323 69 103 50 227 178 280 288
+22 47 46 109 52 139 18 77 16 224 -4 301 -39 150 -145 301 -270 381 -120 78
+-278 120 -391 103z"
+      />
+    </g>
+  </svg>
+);
+
+export default SolidSVG;
